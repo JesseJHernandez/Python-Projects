2eeb26f77097549061ac070460539979685d8aef
JesseJHernandez <jesse.james.hernandez@hotmail.com>
2014-11-13 22:40:45 -0800

    Added Madlibs generator

---
+++ madlibs.py
@@ -0,0 +1,5 @@
+noun = raw_input("Insert Noun ")
+verb = raw_input("Insert Verb ")
+adjective = raw_input("Insert Adjective ")
+
+print "Once upon a time there was a %s who would %s everything in their sight like a %s bird. " % (noun,verb,adjective)
