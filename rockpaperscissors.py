a2f33cbbdad1754f282b8f3511f16a6300fe4065
JesseJHernandez <jesse.james.hernandez@hotmail.com>
2014-11-15 17:37:32 -0800

    Added Rock paper Scissors code

---
+++ rockpaperscissors.py
@@ -0,0 +1,20 @@
+options = ["rock ", "paper ", "scissor "]
+
+#User inputs their choice
+user_input = raw_input("Rock, Paper or Scissors? ")
+if user_input in options:
+	print user_input
+
+#Computer outputs their choice
+import random
+	for i in range(1):
+	computer = random.choice(options)
+	print random.choice(options)
+
+	
+
+	if user_input == options[1] and computer == options[1]:
+		print "It is a draw!"
+
+
+
